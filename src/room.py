470ea2785bbd2323d1477607693d6777cd21f2cb
justinandrade <j.andrade.john@gmail.com>
2019-11-20 00:40:34 -0500

    room.py completed

--- src/room.py
+++ src/room.py
@@ -1,2 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f'Welcome to {self.name}'
+
+    def __repr__(self):
+        return f'Room:({self.name}), Decription:({self.description})'
+
+
+cool_room = Room('Cool', 'A cool room for cool things and cool people.')
+print(cool_room)
